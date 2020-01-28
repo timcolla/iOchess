abd632377e9f3feed7730662e658dc0a376c415c
Tim Colla <tim.colla@marinosoftware.com>
2020-01-28 16:30:18 +0100

    Copy Game logic to GameController

--- Chess/Controllers/GameController.swift
+++ Chess/Controllers/GameController.swift
@@ -10,6 +10,7 @@ import Foundation
 
 class GameController {
     var board: [Piece?]
+    var selectedSquare: Int?
 
     init() {
         var movedPawnBlack = Pawn(colour: .black)
@@ -17,12 +18,119 @@ class GameController {
         var movedPawnWhite = Pawn(colour: .white)
         movedPawnWhite.firstMove = false
         board = [Rook(colour: .black), Knight(colour: .black), Bishop(colour: .black), Queen(colour: .black), King(colour: .black), Bishop(colour: .black), Knight(colour: .black), Rook(colour: .black),
-                 Pawn(colour: .black), Pawn(colour: .black), nil, Pawn(colour: .black), nil, Pawn(colour: .black), Pawn(colour: .black), Pawn(colour: .black),
-                 nil, nil, movedPawnBlack, nil, movedPawnBlack, nil, nil, nil,
+                 Pawn(colour: .black), Pawn(colour: .black), Pawn(colour: .black), Pawn(colour: .black), Pawn(colour: .black), Pawn(colour: .black), Pawn(colour: .black), Pawn(colour: .black),
                  nil, nil, nil, nil, nil, nil, nil, nil,
                  nil, nil, nil, nil, nil, nil, nil, nil,
-                 nil, movedPawnWhite, nil, nil, movedPawnWhite, nil, nil, nil,
-                 Pawn(colour: .white), nil, Pawn(colour: .white), Pawn(colour: .white), nil, Pawn(colour: .white), Pawn(colour: .white), Pawn(colour: .white),
+                 nil, nil, nil, nil, nil, nil, nil, nil,
+                 nil, nil, nil, nil, nil, nil, nil, nil,
+                 Pawn(colour: .white), Pawn(colour: .white), Pawn(colour: .white), Pawn(colour: .white), Pawn(colour: .white), Pawn(colour: .white), Pawn(colour: .white), Pawn(colour: .white),
                  Rook(colour: .white), Knight(colour: .white), Bishop(colour: .white), Queen(colour: .white), King(colour: .white), Bishop(colour: .white), Knight(colour: .white), Rook(colour: .white)]
+    }
+
+    func selectSquare(index: Int) -> Bool {
+        if let selectedSquare = selectedSquare {
+            let possibleSquares = self.possibleSquares(for: index)
+
+            // move piece
+            if possibleSquares.contains(index) {
+                movePiece(from: selectedSquare, to: index)
+            }
+
+            self.selectedSquare = nil
+        } else {
+            if index > 0, index < board.count, board[index] != nil {
+                selectedSquare = index
+                return true
+            }
+        }
+        return false
+    }
+
+    func possibleSquares(for index: Int) -> [Int] {
+        guard let piece = board[index] else {
+            return [Int]()
+        }
+
+        var possibleSquares = [Int]()
+        if let piece = piece as? Pawn {
+            for relativeMove in piece.takes {
+                let possibleSquare = relativeMove + index
+                if possibleSquare >= 0, possibleSquare < board.count, let possiblePiece = board[possibleSquare], possiblePiece.colour != piece.colour {
+                    possibleSquares.append(possibleSquare)
+                }
+            }
+        }
+        for relativeMove in piece.relativeMoves {
+            for i in 1...piece.range {
+                if index % 8 == 0 && (relativeMove == 7 || relativeMove == -9 || relativeMove == -1) {
+                    break
+                }
+                if index % 8 == 7 && (relativeMove == -7 || relativeMove == 9 || relativeMove == 1) {
+                    break
+                }
+                if index / 8 == 0 && (relativeMove == -7 || relativeMove == -9 || relativeMove == -8) {
+                    break
+                }
+                if index / 8 == 7 && (relativeMove == 7 || relativeMove == 9 || relativeMove == 8) {
+                    break
+                }
+                let possibleSquare = relativeMove * i + index
+                let piecePosition = position(from: index)
+                let possiblePosition = position(from: possibleSquare)
+                if let piece = piece as? Knight, piece.validPossibleSquare(possiblePosition, position: piecePosition) {
+                    if let possiblePiece = board[possibleSquare], possiblePiece.colour == piece.colour {
+                        break
+                    }
+                    possibleSquares.append(possibleSquare)
+                    if board[possibleSquare] != nil {
+                        break
+                    }
+                } else if possibleSquare >= 0 && possibleSquare < board.count && !(piece is Knight) {
+                    if let possiblePiece = board[possibleSquare], possiblePiece.colour == piece.colour {
+                        break
+                    }
+                    possibleSquares.append(possibleSquare)
+                    if board[possibleSquare] != nil {
+                        break
+                    }
+                }
+
+                if possibleSquare % 8 == 0 && (relativeMove == 7 || relativeMove == -9 || relativeMove == -1) {
+                    break
+                }
+                if possibleSquare % 8 == 7 && (relativeMove == -7 || relativeMove == 9 || relativeMove == 1) {
+                    break
+                }
+                if possibleSquare / 8 == 0 && (relativeMove == 7 || relativeMove == -9 || relativeMove == -8) {
+                    break
+                }
+                if possibleSquare / 8 == 7 && (relativeMove == -7 || relativeMove == 9 || relativeMove == 8) {
+                    break
+                }
+            }
+        }
+        return possibleSquares
+    }
+
+    func movePiece(from: Int, to: Int) {
+        guard from > 0, to > 0, from < board.count, to < board.count else {
+            return
+        }
+
+        let piece = board[from]
+        board[from] = nil
+        board[to] = piece
+    }
+
+    func row(from index: Int) -> Int {
+        return index / 8
+    }
+
+    func column(from index: Int) -> Int {
+        return index % 8
+    }
+
+    func position(from index: Int) -> (column: Int, row: Int) {
+        return (column(from: index), row(from: index))
     }
 }
