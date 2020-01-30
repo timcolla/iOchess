2cdfc0ef1efa7527992a46adb5f41748a0905ef4
Tim Colla <tim.colla@marinosoftware.com>
2020-01-30 14:11:28 +0100

    Push failing test

--- ChessTests/ChessTests.swift
+++ ChessTests/ChessTests.swift
@@ -21,6 +21,7 @@ class ChessTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(false)
     }
 
     func testPerformanceExample() {
