99e93c509ab44cdbcfe4e0949885e865bbf4625f
Tim Colla <tim.colla@marinosoftware.com>
2020-01-30 15:38:28 +0100

    Add (hopefully) last push test

--- ChessTests/ChessTests.swift
+++ ChessTests/ChessTests.swift
@@ -21,7 +21,7 @@ class ChessTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssert([1].count == 1)
+        XCTAssert(true)
     }
 
     func testPerformanceExample() {
