0b8a94dc0503339371b78fbdf1e322db0f61d716
Tim Colla <tim.colla@marinosoftware.com>
2020-01-30 14:41:56 +0100

    Push succeeding test and setup upstream

--- ChessTests/ChessTests.swift
+++ ChessTests/ChessTests.swift
@@ -21,7 +21,7 @@ class ChessTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssert(true)
+        XCTAssert([1].count == 1)
     }
 
     func testPerformanceExample() {
